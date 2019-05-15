524135da56f48a1764b6cf996a6920e173a47815
Kathy <kathrcardoza@gmail.com>
2019-05-14 20:57:46 -0700

    adding timer

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,5 +6,8 @@ module.exports = {
     },
     userHighScores: function(uid, name) {
         return axios.get('/api/score/user/' + uid + "/" + name);
+    },
+    postMineSweeper: function(score, firebaseuid) {
+        return axios.post ('/api/score/score', {gameName: 'minesweeper', score: score, uid:firebaseuid })
     }
 };

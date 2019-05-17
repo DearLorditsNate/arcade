df5bc7f8c2f440d11fd4477f363415028b9f418b
Nate Micinski <nmicinski@gmail.com>
2019-05-16 18:32:04 -0700

    removed navbar state change flash

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../App.css";
-import SignInForm from "../SignInForm";
 import SignOutBtn from "../SignOutBtn";
 import AccountPageBtn from "../AccountPageBtn";
 import SignUpModal from "../SignUpModal";
@@ -10,9 +9,58 @@ import { AuthUserContext } from "../Session";
 const Navbar = () => (
   <div>
     <AuthUserContext.Consumer>
-      {authUser => authUser ? <NavigationAuth uid={authUser.uid} /> : <NavigationNonAuth />}
+      {authUser => {
+        if (authUser === null) {
+          return <NavigationNeutral />
+        } else if (authUser === false) {
+          return <NavigationNonAuth />
+        } else {
+          return <NavigationAuth />
+        }
+      }
+    }
     </AuthUserContext.Consumer>
   </div>
+);
+
+const NavigationNeutral = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="/">
+      <img
+        src="https://img06.deviantart.net/319c/i/2011/073/4/d/8bit_mario_by_anone52-d3bmhtj.jpg"
+        width="30"
+        height="50"
+        className="d-inline-block align-middle mr-2"
+        alt=""
+      />
+      Retro Arcade
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            Game List
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/highscores">
+            Global High Scores
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 );
 
 const NavigationAuth = props => (
@@ -40,9 +88,9 @@ const NavigationAuth = props => (
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
-        <li className="nav-item active">
+        <li className="nav-item">
           <a className="nav-link" href="/">
-            Game List <span className="sr-only">(current)</span>
+            Game List
           </a>
         </li>
         <li className="nav-item">
@@ -83,9 +131,9 @@ const NavigationNonAuth = () => (
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
-        <li className="nav-item active">
+        <li className="nav-item">
           <a className="nav-link" href="/">
-            Game List <span className="sr-only">(current)</span>
+            Game List
           </a>
         </li>
         <li className="nav-item">

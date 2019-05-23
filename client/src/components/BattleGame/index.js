51b872d20cd9bc654afe45ad6c9b1e3b51fcbc29
Nate Micinski <nmicinski@gmail.com>
2019-05-22 20:12:46 -0700

    battlegame is now mobile responsive

--- client/src/components/BattleGame/index.js
+++ client/src/components/BattleGame/index.js
@@ -1,54 +1,66 @@
 import React from "react";
-import './style.css'
+import "./style.css";
 import { AuthUserContext } from "../Session";
 
 const battlegame = () => (
   <AuthUserContext.Consumer>
-    {authUser => (authUser ? <Battlegame uid={authUser.uid} /> : <Battlegame />)}
+    {authUser =>
+      authUser ? <Battlegame uid={authUser.uid} /> : <Battlegame />
+    }
   </AuthUserContext.Consumer>
-)
+);
 
 class Battlegame extends React.Component {
   componentDidMount() {
-
     let script = document.createElement("script");
-
 
     script.src = "./js/battlegame/battlegame.js";
 
     //check if the script tags have not yet been added
-    let scriptTags = document.getElementsByTagName('script');
+    let scriptTags = document.getElementsByTagName("script");
     let scriptSources = [];
     for (var i = 0; i < scriptTags.length; i++) {
       scriptSources.push(scriptTags[i].outerHTML);
     }
 
     //turn script tage into strings so they can strictly match the scriptSources array
-    let scriptstring = `<script src="./js/battlegame/battlegame.js"></script>`
+    let scriptstring = `<script src="./js/battlegame/battlegame.js"></script>`;
 
     if (scriptSources.indexOf(scriptstring) === -1) {
-
       document.body.appendChild(script);
     }
   }
   render() {
     return (
       <div>
-        <canvas id="battleGameCanvas" width="450" height="450" data-id={this.props.uid}></canvas>
+        <canvas
+          id="battleGameCanvas"
+          width="450"
+          height="450"
+          data-id={this.props.uid}
+        />
         <div id="battleGamewrapper" data-id={this.props.uid}>
-        <div id="container">
-          <button id="beginGame">Begin Game</button>
-          <button id="sendMissile">Send Missile</button>
+          <div id="container">
+            <button id="beginGame">Begin Game</button>
+            <button id="sendMissile">Send Missile</button>
+
+            {/* <div id="container"> */}
+            <div id="sunken">
+              BattleShips Sunk: <div id="sink">#</div>
             </div>
-            <div id="container">
-            <div id="sunken">BattleShips Sunk: <div id="sink">#</div></div>
-            <div id="missilesFired">Missiles Fired: <div id="fired" >#</div></div>
-            <div id="hits">Hits: <div id="hit" >#</div></div>
+            <div id="missilesFired">
+              Missiles Fired: <div id="fired">#</div>
+            </div>
+            <div id="hits">
+              Hits: <div id="hit">#</div>
+            </div>
             <a href="/battlegame">
-              <button className="d-block m-auto" id=
-              "battleGameResetButton">RESET</button>
+              <button className="d-block m-auto" id="battleGameResetButton">
+                RESET
+              </button>
             </a>
-             {/* Save Score Modal */}
+          </div>
+          {/* Save Score Modal */}
           <div
             className="modal fade"
             id="battlegame-save-modal"
@@ -63,7 +75,10 @@ class Battlegame extends React.Component {
             >
               <div className="modal-content text-center">
                 <div className="modal-header">
-                  <h5 className="modal-title" id="battlegame-save-modal-title" />
+                  <h5
+                    className="modal-title"
+                    id="battlegame-save-modal-title"
+                  />
                   <button
                     type="button"
                     className="close"
@@ -121,15 +136,8 @@ class Battlegame extends React.Component {
             </div>
           </div>
         </div>
-          </div>
-          </div>
-         
-          
-      
-
-
-     
-
+      </div>
+      // </div>
     );
   }
 }
